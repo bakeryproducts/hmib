3911cd22592e4689b24c228c58a65db1c9afbcce
bakeryproducts <gleb.m.sokolov@gmail.com>
2022-08-18 20:55:11 +0300

    some modifications on preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,8 @@
-import os
-import os.path as osp
-from argparse import ArgumentParser
+#!/usr/bin/python3
 
+from pathlib import Path
+
+import fire
 import cv2
 import numpy as np
 import pandas as pd
@@ -21,87 +22,79 @@ def rle_decode(rle_str, mask_shape, mask_dtype=np.uint8):
     return mask.reshape(mask_shape[::-1]).T
 
 
-def create_if_not_exist(dirpath):
-    if not osp.exists(dirpath):
-        os.makedirs(dirpath)
-
-
 def crop_generator(size, crop_size, crop_step):
     for h_start in range(0, size - crop_size + 1, crop_step):
         for w_start in range(0, size - crop_size + 1, crop_step):
             yield h_start, w_start, h_start + crop_size, w_start + crop_size
 
 
-def parse_args():
-    parser = ArgumentParser()
-
-    parser.add_argument("--size", type=int, default=1024, help="Image size after resize")
-    parser.add_argument("--crop_size", type=int, default=512)
-    parser.add_argument("--crop_step", type=int, default=256)
-    parser.add_argument("--data_dir", type=str, default="./input")
-
-    return parser.parse_args()
+def check_sizes(crop_size, size, crop_step):
+    assert crop_size > 0, f"crop_size <= 0: {crop_size}"
+    assert crop_size <= size, (crop_size < size)
+    assert (size - crop_size) % crop_step == 0, "crop_size and crop_step should cover full image"
 
 
-def check_sizes(args):
-    if args.crop_size <= 0:
-        raise ValueError(f"crop_size <= 0: {args.crop_size}")
+def folder_gen(src):
+    images = list((src / 'images').rglob('*.tiff'))
+    print(f'\n\ttotal length: {len(images)}')
+    for ifn in images:
+        image = load_tiff(str(ifn))
+        mfn = src / 'masks' / ifn.name
+        masks = load_tiff(str(mfn))
+        yield ifn.stem, image, masks
 
-    if args.crop_size > args.size:
-        raise ValueError(f"crop_size > size")
 
-    if (args.size - args.crop_size) % args.crop_step != 0:
-        raise ValueError(f"crop_size and crop_step should cover full image")
+def df_hubmap2(src):
+    ori_images_dir = src / "train_images"
+    ori_train_csv = src / "train.csv"
+    df = pd.read_csv(ori_train_csv)
+    print(f'\n\ttotal length: {len(df)}')
+
+    for i, row in df.iterrows():
+        name = row.id
+        image = load_tiff(str(ori_images_dir / f"{name}.tiff"))
+        mask = rle_decode(row.rle, image.shape[:2])
+        yield name, image, mask
 
 
-def main(args):
-    check_sizes(args)
+def do_cuts(src='input/hmib', dst='input/preprocessed', size=1024, crop_size=512, crop_step=256):
+    check_sizes(crop_size, size, crop_step)
+    subcrops = size != crop_size
+    src = Path(src)
+    dst = Path(dst)
 
-    data_dir = args.data_dir
-    subcrops = args.size != args.crop_size
-
-    # Original data paths
-    ori_data_dir = osp.join(data_dir, "hmib")
-    ori_images_dir = osp.join(ori_data_dir, "train_images")
-    ori_train_csv = osp.join(ori_data_dir, "train.csv")
-
-    # Output directory name
-    output_dirname = f"rle_{args.size}"
+    output_dirname = f"rle_{size}"
     if subcrops:
-        overlap = args.crop_size - args.crop_step
-        output_dirname += f"_c{args.crop_size}_o{overlap}"
+        overlap = crop_size - crop_step
+        output_dirname += f"_c{crop_size}_o{overlap}"
 
     # Output directories
-    output_data_dir = osp.join(args.data_dir, "preprocessed", output_dirname)
-    output_images_dir = osp.join(output_data_dir, "images")
-    output_masks_dir = osp.join(output_data_dir, "masks")
+    output_data_dir = dst / output_dirname
+    output_images_dir = output_data_dir / "images"
+    output_masks_dir = output_data_dir / "masks"
+    output_images_dir.mkdir(exist_ok=False, parents=True)
+    output_masks_dir.mkdir(exist_ok=False, parents=True)
 
-    create_if_not_exist(output_images_dir)
-    create_if_not_exist(output_masks_dir)
 
-    # Load train csv
-    df = pd.read_csv(ori_train_csv)
+    gen = df_hubmap2(src)
+    # gen = folder_gen(src)
 
     # Preprocess images
-    for row in tqdm(df.itertuples(), total=len(df), desc="Preprocessing images"):
-        # Load image and mask
-        image = load_tiff(osp.join(ori_images_dir, f"{row.id}.tiff"))
-        mask = rle_decode(row.rle, image.shape[:2])
-
+    for name, image, mask in tqdm(gen):
         # Resize
-        image = cv2.resize(image, (args.size, args.size), interpolation=cv2.INTER_CUBIC)
-        mask = cv2.resize(mask, (args.size, args.size), interpolation=cv2.INTER_CUBIC)
+        image = cv2.resize(image, (size, size), interpolation=cv2.INTER_CUBIC)
+        mask = cv2.resize(mask, (size, size), interpolation=cv2.INTER_CUBIC)
 
         # Save
         if subcrops:
-            for i, (y1, x1, y2, x2) in enumerate(crop_generator(args.size, args.crop_size, args.crop_step)):
-                cv2.imwrite(osp.join(output_images_dir, f"{row.id}_{i}.png"), image[y1: y2, x1: x2])
-                cv2.imwrite(osp.join(output_masks_dir, f"{row.id}_{i}.png"), mask[y1: y2, x1: x2])
+            for i, (y1, x1, y2, x2) in enumerate(crop_generator(size, crop_size, crop_step)):
+                cv2.imwrite(str(output_images_dir / f"{name}_{i}.png"), image[y1: y2, x1: x2])
+                cv2.imwrite(str(output_masks_dir / f"{name}_{i}.png"), mask[y1: y2, x1: x2])
 
         else:
-            cv2.imwrite(osp.join(output_images_dir, f"{row.id}.png"), image)
-            cv2.imwrite(osp.join(output_masks_dir, f"{row.id}.png"), mask)
+            cv2.imwrite(str(output_images_dir / f"{name}.png"), image)
+            cv2.imwrite(str(output_masks_dir / f"{name}.png"), mask)
 
 
 if __name__ == "__main__":
-    main(parse_args())
+    fire.Fire(do_cuts)
