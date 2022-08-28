46671024c2c751a45bc942d0b08de2d7ad449294
Alexander Snorkin <Alexander.Snorkin@acronis.com>
2022-08-28 16:31:54 +0400

    rle_encode fix: .T

--- src/mask_utils.py
+++ src/mask_utils.py
@@ -40,7 +40,7 @@ def rle_encode(img):
         run length as string formated
     """
 
-    pixels = img.flatten()
+    pixels = img.T.flatten()
     pixels = np.concatenate([[0], pixels, [0]])
     runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
     runs[1::2] -= runs[::2]
