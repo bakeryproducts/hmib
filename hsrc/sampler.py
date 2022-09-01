875f56fb9cb2f0cf589c7e966f42fff2c55a874c
ubuntu <bla@bla.bla>
2022-09-01 16:43:37 +0400

    hsrc/sampler.py random.shuffle seed fix

--- hsrc/sampler.py
+++ hsrc/sampler.py
@@ -38,7 +38,8 @@ class GdalSampler:
         polygons = flatten_2dlist([json_record_to_poly(record) for record in self._records_json])
 
         if shuffle:
-            random.shuffle(polygons, seed=42)
+            random.seed(42)
+            random.shuffle(polygons)
 
         self._polygons_centroid = [np.round(polygon.centroid) for polygon in polygons]
 
