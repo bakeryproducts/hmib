4b4bcab2cf147796c68d3f08727bfce09dc5211c
bakeryproducts <gleb.m.sokolov@gmail.com>
2022-09-14 20:46:04 +0300

    body annotation sync

--- src/scripts/prepgtex.sh
+++ src/scripts/prepgtex.sh
@@ -7,3 +7,8 @@ python3 hsrc/data_gen.py --src input/extra/gtex/pieces/prostate --dst temp/$DST/
 python3 hsrc/data_gen.py --src input/extra/gtex/pieces/kidney --dst temp/$DST/gtex_$SCALE/kidney --src_scale .5 --dst_scale $SCALE --cropsize $CROP --recursive --mode grid
 python3 hsrc/data_gen.py --src input/extra/gtex/pieces/spleen --dst temp/$DST/gtex_$SCALE/spleen --src_scale .5 --dst_scale $SCALE --cropsize $CROP --recursive --mode inst
 python3 hsrc/data_gen.py --src input/extra/gtex/pieces/colon --dst temp/$DST/gtex_$SCALE/colon --src_scale .5 --dst_scale $SCALE --cropsize $CROP --recursive --mode grid
+
+# python3 hsrc/data_gen.py --src input/extra/gtex/pieces/prostate_test --dst temp/$DST/gtex_${SCALE}_test/prostate --src_scale .5 --dst_scale $SCALE --cropsize $CROP --recursive --mode grid
+python3 hsrc/data_gen.py --src input/extra/gtex/pieces/kidney_test --dst temp/$DST/gtex_${SCALE}_test/kidney --src_scale .5 --dst_scale $SCALE --cropsize $CROP --recursive --mode grid
+python3 hsrc/data_gen.py --src input/extra/gtex/pieces/spleen_test --dst temp/$DST/gtex_${SCALE}_test/spleen --src_scale .5 --dst_scale $SCALE --cropsize $CROP --recursive --mode inst
+python3 hsrc/data_gen.py --src input/extra/gtex/pieces/colon_test --dst temp/$DST/gtex_${SCALE}_test/colon --src_scale .5 --dst_scale $SCALE --cropsize $CROP --recursive --mode grid
